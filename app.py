50833097b38fa8fbe7a776d89d617e38ca330395
Diego Batista <alpha.diegobatista@gmail.com>
2022-05-04 23:00:02 -0300

    Desafio: Custom Message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Diego Batista - https://github.com/apokalypsi/devopslab-7aso"
 
 if __name__ == '__main__':
     app.run()
