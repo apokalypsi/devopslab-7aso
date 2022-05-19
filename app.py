469c23f45923541fd0b1dcbb955046b9ee5879c8
Diego Batista <alpha.diegobatista@gmail.com>
2022-05-18 23:18:24 -0300

    Bug Added
    Bug Added

--- app.py
+++ app.py
@@ -9,5 +9,14 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Diego Batista - https://github.com/apokalypsi/devopslab-7aso"
 
+@app.route('/bug')                                                                                                                                
+def bad():                                                                                                                                        
+    try:                                                                                                                                          
+        raise TypeError()                                                                                                                         
+    except TypeError as e:                                                                                                                        
+        print(e)                                                                                                                                  
+    except TypeError as e:                                                                                                                        
+        print("Duplicado, ou seja, nunca vai entrar aqui.")    
+
 if __name__ == '__main__':
     app.run()
