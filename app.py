903c9116ef5766b040ab798e30648a6575369c5a
Diego Batista <alpha.diegobatista@gmail.com>
2022-06-08 21:04:15 -0300

    Desafio: "Dockerizar" a App

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ def pagina_inicial():
     return "Diego Batista - https://github.com/apokalypsi/devopslab-7aso"  
 
 if __name__ == '__main__':
+    import os
     port = os.getenv('PORT')
     app.run('0.0.0.0', port=port)
